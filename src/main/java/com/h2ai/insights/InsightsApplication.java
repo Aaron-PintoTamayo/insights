2e056953d9b73c26adb3e2fbe4251c25d47b7c0c
Aaron Pinto <pintotamayoaaron@gmail.com>
2026-04-10 18:32:38 -0400

    basic structure is done

---
+++ src/main/java/com/h2ai/insights/InsightsApplication.java
@@ -0,0 +1,12 @@
+package com.h2ai.insights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightsApplication.class, args);
+    }
+}
